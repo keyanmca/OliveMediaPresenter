1cd4f3a7545ab55708eccb3f1c36666ab6641474
Amos A. Anderson <amosanderson@gmail.com>
2014-03-30 15:55:21 -0500

    Clean up
    Cleaned up source and added some comments.
    Added .avi detection.
    Made readme more clear.

--- MediaPresenter/OliveMediaPlayer/MainWindow.xaml.cs
+++ MediaPresenter/OliveMediaPlayer/MainWindow.xaml.cs
@@ -13,27 +13,27 @@ namespace OliveMediaPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Dictionary<System.Int32, System.Uri> fileList = new Dictionary<System.Int32, System.Uri>();
+        private Dictionary<Int32, Uri> fileList = new Dictionary<Int32, Uri>(); // list of video files
 
-        private Int32 nextVideo = 1;
+        private Int32 nextVideo = 1; // current video position
 
-        private bool loop = false;
+        private bool loop = false; // Should the video loop? Default is no
 
         public MainWindow()
         {
             InitializeComponent();
 
-            LoadFiles();
+            LoadFiles();    // on startup load all of the found video files
         }
 
-        private void LoadFiles()
+        private void LoadFiles()    // loads the video files 
         {
-            String videoDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "Presentation");
+            String videoDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "Presentation"); // On Windows 7 it would be C:\users\<username>\Video\Presentation to get the files from
 
             if (System.IO.Directory.Exists(videoDir))
             {
                 var files = from p in System.IO.Directory.GetFiles(videoDir)
-                            where Path.GetExtension(p) == ".wmv" || Path.GetExtension(p) == ".mp4" || Path.GetExtension(p) == ".m4v"
+                            where Path.GetExtension(p) == ".wmv" || Path.GetExtension(p) == ".mp4" || Path.GetExtension(p) == ".m4v" || Path.GetExtension(p) == ".avi"
                             orderby p
                             select p;
 
@@ -45,12 +45,12 @@ namespace OliveMediaPlayer
                     fileList.Add(key++, new Uri(i));
 
                 if (fileList.Count > 1)
-                    mainMediaElement.Source = fileList[1];
+                    PlayerMediaElement.Source = fileList[1];
             }
             else
             {
                 System.IO.Directory.CreateDirectory(videoDir);
-                MessageBox.Show("Please put your videos in a folder named \"Presentation\" inside your \"My Videos\" folder. Here, I'll open it for you.", "CBC Media Presenter");
+                MessageBox.Show("Please put your videos in a folder named \"Presentation\" inside your \"My Videos\" folder. Here, I'll open it for you.", "Olive Media Player");
                 Process.Start(videoDir);
                 CloseApplication();
             }
@@ -60,102 +60,102 @@ namespace OliveMediaPlayer
         {
             switch (e.Key)
             {
-                case Key.D1:
+                case Key.D1:    // 1 pressed: load file 1
                     if (fileList.Count >= 1)
                     {
                         loop = false;
                         nextVideo = 1;
                     }
                     break;
-                case Key.D2:
+                case Key.D2:    // 2 pressed: load file 2
                     if (fileList.Count >= 2)
                     {
                         loop = false;
                         nextVideo = 2;
                     }
                     break;
-                case Key.D3:
+                case Key.D3:    // 3 pressed: load file 3
                     if (fileList.Count >= 3)
                     {
                         loop = false;
                         nextVideo = 3;
                     }
                     break;
-                case Key.D4:
+                case Key.D4:    // 4 pressed: load file 4
                     if (fileList.Count >= 4)
                     {
                         loop = false;
                         nextVideo = 4;
                     }
                     break;
-                case Key.D5:
+                case Key.D5:    // 5 pressed: load file 5
                     if (fileList.Count >= 5)
                     {
                         loop = false;
                         nextVideo = 5;
                     }
                     break;
-                case Key.D6:
+                case Key.D6:    // 6 pressed: load file 6
                     if (fileList.Count >= 6)
                     {
                         loop = false;
                         nextVideo = 6;
                     }
                     break;
-                case Key.D7:
+                case Key.D7:    // 7 pressed: load file 7
                     if (fileList.Count >= 7)
                     {
                         loop = false;
                         nextVideo = 7;
                     }
                     break;
-                case Key.D8:
+                case Key.D8:    // 8 pressed: load file 8
                     if (fileList.Count >= 8)
                     {
                         loop = false;
                         nextVideo = 8;
                     }
                     break;
-                case Key.D9:
+                case Key.D9:    // 9 pressed: load file 9
                     if (fileList.Count >= 9)
                     {
                         loop = false;
                         nextVideo = 9;
                     }
                     break;
-                case Key.D0:
+                case Key.D0:    // 0 pressed: load file 10
                     if (fileList.Count >= 10)
                     {
                         loop = false;
                         nextVideo = 10;
                     }
                     break;
-                case Key.L:
+                case Key.R:     // R pressed: reload all files
                     LoadFiles();
                     break;
-                case Key.X:
+                case Key.L:     // L pressed: enable single file loop
                     loop = !loop;
                     break;
-                case Key.Space:
-                    if (mainMediaElement.Source == fileList[nextVideo])
+                case Key.Space: // space pressed: start next element if one is selected, otherwise start or stop media
+                    if (PlayerMediaElement.Source == fileList[nextVideo])
                     {
-                        mainMediaElement.Play();
+                        PlayerMediaElement.Play();
                     }
                     else
                     {
-                        mainMediaElement.Stop();
-                        mainMediaElement.Close();
-                        mainMediaElement.Source = fileList[nextVideo];
-                        mainMediaElement.Play();
+                        PlayerMediaElement.Stop();
+                        PlayerMediaElement.Close();
+                        PlayerMediaElement.Source = fileList[nextVideo];
+                        PlayerMediaElement.Play();
                     }
                     break;
-                case Key.S:
+                case Key.S: // S pressed: stop media playback
                     loop = false;
-                    mainMediaElement.Stop();
-                    mainMediaElement.Close();
+                    PlayerMediaElement.Stop();
+                    PlayerMediaElement.Close();
                     break;
-                case Key.P:
-                    mainMediaElement.Pause();
+                case Key.P: // P pressed: pause media playback
+                    PlayerMediaElement.Pause();
                     break;
                 case Key.Escape:        // close if escape is pressed
                     this.CloseApplication();
@@ -165,16 +165,16 @@ namespace OliveMediaPlayer
             }
         }
 
-        private void mainMediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        private void PlayerMediaElement_MediaEnded(object sender, RoutedEventArgs e)
         {
-            if (loop)
+            if (loop)   // loop if selected
             {
-                mainMediaElement.Position = new TimeSpan(0, 0, 0);
-                mainMediaElement.Play();
+                PlayerMediaElement.Position = new TimeSpan(0, 0, 0);
+                PlayerMediaElement.Play();
             }
-            else
+            else        // close media when playback ended if loop is not selected
             {
-                mainMediaElement.Close();
+                PlayerMediaElement.Close();
             }
         }
 
--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 OliveMediaPresenter
 ===================
 
-Software for remote/local digital presentation and digital signage.
+Software for digital presentation and digital signage. It allows you present videos in a professional manner. This application gives you total control of your playlist without showing the audience the playing and switching of the videos. This application will also loop videos for digital signage. 
 
-Place .mk4, .avi and .wmv files in C:\Users\<username>\Videos\Presentation
+Future Goal: To permit remote control of multiple digital signage devices on the same local network. 
 
-On startup the application will load the files in alphabetical order starting at 1. I recommend starting the names with numbers like: 01, 02, 03, 04, 05, and so on. 
+## Instructions ##
+Place .mk4, .avi and .wmv files in C:\\Users\\<username>\\Videos\\Presentation 
 
-Numbers 0-9: Queue from files in order from 1 - 10. 0 represending the 10th video file. 
-Space Bar: Play Queued Video
-S : Stop Playing
-P : Pause Playing
+**Note:** this software will only play files that uses codecs that are installed on the computer. If you can play it in Windows Media Player on that computer then this software should also play it. 
+
+On start up the application will load the files in alphabetical order starting at 1. I recommend starting the names with numbers like: 01, 02, 03, 04, 05, and so on. 
+
+### Keys ###
+
+Numbers 0-9: Queue from files in order from 1 - 10. 0 representing the 10th video file. 
+
+Space Bar: Play queued video is stopped, load and play next queued video is one is queued
+
+S : Stop playing
+
+P : Pause playing
+
 L : Reload files from the "Presentation" folder in "My Videos"
-ESC: Close Application
 
-Note: Place the files in the directory in alphabetical order in which you want them to load on your number keys.
+X : Enables looping of the currently queued video. It does not automatically start that video playing.  
+
+ESC: Close application
